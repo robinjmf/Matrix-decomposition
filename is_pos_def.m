1dffdbf77ecc1118fdcac11144810f889b669fcf
robinjmf <robn.jmf@gmail.com>
2021-11-20 14:30:00 +0330

    update pos_definite

--- is_pos_def.m
+++ is_pos_def.m
@@ -1,13 +1,13 @@
 function f = is_pos_def(input)
     eig_A = eig(input);
-    flag = 0;
+    flag = 1;
     for i = 1:rank(input)
         if eig_A(i) <= 0
-            flag = 1;
+            flag = 0;
         end
     end
     f = flag;
-    if flag == 1
+    if flag == 0
         disp('the matrix is not positive definite')
     else
         disp('the matrix is positive definite')
